644d636e024f43b065ef320cb0aae89b99b1ec6d
TrungCrit <Trung10101995@gmail.com>
2020-09-22 10:41:06 +0700

    First commit

---
+++ EasyLoginTalkManager/EasyLoginTalkManager/EasyLoginTalkManager.h
@@ -0,0 +1,19 @@
+//
+//  EasyLoginTalkManager.h
+//  EasyLoginTalkManager
+//
+//  Created by Nguyễn Trung on 9/22/20.
+//  Copyright © 2020 Nguyễn Trung. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EasyLoginTalkManager.
+FOUNDATION_EXPORT double EasyLoginTalkManagerVersionNumber;
+
+//! Project version string for EasyLoginTalkManager.
+FOUNDATION_EXPORT const unsigned char EasyLoginTalkManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyLoginTalkManager/PublicHeader.h>
+
+
---
+++ EasyLoginTalkManager/EasyLoginTalkManager/EasyLoginTalkManager.swift
@@ -0,0 +1,19 @@
+//
+//  EasyLoginTalkManager.swift
+//  EasyLoginTalkManager
+//
+//  Created by Nguyễn Trung on 9/22/20.
+//  Copyright © 2020 Nguyễn Trung. All rights reserved.
+//
+
+import Foundation
+
+public class EasyLoginTalkManager {
+
+    private init() {}
+    
+    public static func callCanLogin() -> Bool {
+        return true
+    }
+    
+}
